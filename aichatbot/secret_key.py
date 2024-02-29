17c5c1b550d7aa83b7a478b17ae05aeace9cb1ab
Hanna Lam <hannalam.momo@gmail.com>
2024-02-29 18:45:49 +0100

    secret key note

--- aichatbot/secret_key.py
+++ aichatbot/secret_key.py
@@ -1,1 +1,3 @@
 API_KEY = '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'
+
+# The function will not work as I publish this on Github, but it work locally as I use another API_KEY for video demo
